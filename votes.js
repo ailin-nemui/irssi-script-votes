79338679ab32f6544e0857630ba1f410d0a6934d
Irssi Votes Helper <votes@irssi.org>
2024-01-24 23:19:47 +0000

    Votes from Wed Jan 24 23:19:47 UTC 2024

--- votes.js
+++ votes.js
@@ -2047,7 +2047,7 @@ addVotes({
    },
    "trackbar.pl" : {
       "u" : "https://github.com/ailin-nemui/irssi-script-votes/issues/6#issuecomment-242195542",
-      "v" : 13
+      "v" : 14
    },
    "trackbar22.pl" : {
       "u" : "https://github.com/ailin-nemui/irssi-script-votes/issues/6#issuecomment-242198427",
